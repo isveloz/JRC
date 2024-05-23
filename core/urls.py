3dac04509c281dfd95eb59491344a2746ad283fb
isveloz <isaiasveloz41@gmail.com>
2024-05-22 21:42:52 -0400

    cambios visuales, mañana terminar htmls y modificar base de datos e implementar vistas de bodega, contador. Finalmente APIS

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path # type: ignore
+from django.urls import path
 from . import views
 
 urlpatterns = [
@@ -6,4 +6,14 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('productos/', views.productos, name='productos'),
     path('contacto/', views.contacto, name='contacto'),
+    path('estanley-esmeril/', views.estanley_esmeril, name='estanley_esmeril'),
+    path('estanley-sierra/', views.estanley_sierra, name='estanley_sierra'),
+    path('soldadora-indurarc/', views.soldadora_indurarc, name='soldadora_indurarc'),
+    path('volcanita/', views.volcanita, name='volcanita'),
+    path('teja/', views.teja, name='teja'),
+    path('volcan-rollo-lana/', views.volcan_rollo_lana, name='volcan_rollo_lana'),
+    path('volcan-huincha/', views.volcan_huincha, name='volcan_huincha'),
+    path('cadena/', views.cadena, name='cadena'),
+    path('inchalam-malla/', views.inchalam_malla, name='inchalam_malla'),
+    path('calcular_precio/', views.calcular_precio, name='calcular_precio'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
-from django.core.paginator import Paginator # type: ignore
-from django.shortcuts import render # type: ignore
+import locale
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Producto
+
+# Configurar la localización para CLP
+locale.setlocale(locale.LC_ALL, 'es_CL.UTF-8')
 
 def home(request):
     return render(request, 'core/home.html')
@@ -9,11 +14,49 @@ def about(request):
     return render(request, 'core/about.html')
 
 def productos(request):
-    productos_list = Producto.objects.all().order_by('nombre')  # Actualiza 'Nombre' a 'nombre'
-    paginator = Paginator(productos_list, 6)
+    productos_list = Producto.objects.all().order_by('nombre')
+    paginator_instance = Paginator(productos_list, 6)
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginator_instance.get_page(page_number)
     return render(request, 'core/productos.html', {'page_obj': page_obj})
 
 def contacto(request):
     return render(request, 'core/contacto.html')
+
+def estanley_esmeril(request):
+    return render(request, 'core/estanley_esmeril.html')
+
+def estanley_sierra(request):
+    return render(request, 'core/estanley_sierra.html')
+
+def soldadora_indurarc(request):
+    return render(request, 'core/soldadora_indurarc.html')
+
+def volcanita(request):
+    return render(request, 'core/volcanita.html')
+
+def teja(request):
+    return render(request, 'core/teja.html')
+
+def volcan_rollo_lana(request):
+    return render(request, 'core/volcan_rollo_lana.html')
+
+def volcan_huincha(request):
+    return render(request, 'core/volcan_huincha.html')
+
+def cadena(request):
+    return render(request, 'core/cadena.html')
+
+def inchalam_malla(request):
+    return render(request, 'core/inchalam_malla.html')
+
+def calcular_precio(request):
+    cantidad = int(request.GET.get('cantidad', 1))
+    precio_unitario = float(request.GET.get('precio_unitario', 0))
+    total = cantidad * precio_unitario
+
+    # Formatear el total en CLP
+    total_clp = locale.format_string("%.0f", total, grouping=True)
+    
+    response_data = {'total': total_clp}
+    return JsonResponse(response_data)
