b33a2eceda59156c7e29a0ba3916d34379120964
isveloz <isaiasveloz41@gmail.com>
2024-05-25 04:47:27 -0400

    Añadir cambios en mysite y nuevos archivos en staticfiles

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,7 @@
-from django.contrib import admin # type: ignore
-from django.urls import path, include # type: ignore
+# mysite/urls.py o jrc/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
